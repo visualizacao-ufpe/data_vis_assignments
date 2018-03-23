1eebd4ab3b31f8be50091c06710599511d2731a0
Nivan Ferreira <nivan.ferreira@gmail.com>
2018-03-22 21:26:10 -0300

    lista 2018.1

---
+++ 2018.1/datasetProblema2.js
@@ -0,0 +1,13 @@
+var dataset = [
+            {"year": 2006, "books": 54, "favoriteGenre":"romance"},
+            {"year": 2007, "books": 43, "favoriteGenre":"policial"},
+            {"year": 2008, "books": 41, "favoriteGenre":"biografia"},
+            {"year": 2009, "books": 44, "favoriteGenre":"policial"},
+            {"year": 2010, "books": 35, "favoriteGenre":"ficcao"},
+	    {"year": 2011, "books": 14, "favoriteGenre":"biografia"},
+            {"year": 2012, "books": 10, "favoriteGenre":"conto"},
+            {"year": 2013, "books": 11, "favoriteGenre":"cronica"},
+	    {"year": 2014, "books":  5, "favoriteGenre":"poesia"},
+            {"year": 2015, "books": 20, "favoriteGenre":"poesia"},
+            {"year": 2016, "books": 35, "favoriteGenre":"poesia"},
+	   ];
